4e8eea171d96bc9e73fc3b6bd932d051803a0835
adiyogta <adiyogta@gmail.com>
2020-04-16 21:43:42 +0700

    CRUD

--- app/src/main/java/com/example/exercisesql_lite/DBHelper.java
+++ app/src/main/java/com/example/exercisesql_lite/DBHelper.java
@@ -12,70 +12,102 @@ import java.util.HashMap;
 
 public class DBHelper extends SQLiteOpenHelper {
 
-    public static final String DATABASE_NAME = "MyDBName.db";
-    public static final String MHS_TABLE_NAME = "kontak";
-    public static final String MHS_COLUMN_ID = "id";
-    public static final String MHS_COLUMN_NAMA = "nama";
-    public static final String MHS_COLUMN_PHONE = "phone";
-    public static final String MHS_COLUMN_EMAIL = "email";
-    public static final String MHS_COLUMN_ALAMAT = "alamat";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String DATABASE_NAME = "db_kontak";
+    private static final String TABLE_NAME = "tb_kontak";
+
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_NAMA = "nama";
+    public static final String COLUMN_PHONE = "phone";
+    public static final String COLUMN_EMAIL = "email";
+    public static final String COLUMN_ALAMAT = "alamat";
 
     private HashMap hp;
 
     public DBHelper(Context context) {
-        super(context, DATABASE_NAME , null, 1);
+        super(context, DATABASE_NAME , null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db){
-        db.execSQL(
-                "create table kontak " +
-                        "(id integer primary key, nama text, phone text, email text, alamat text)"
-        );
+        dataAwal(db);
+    }
+
+    private void dataAwal(SQLiteDatabase db) {
+        String CREATE_KONTAK_TABLE = "CREATE TABLE " + TABLE_NAME + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY," + COLUMN_NAMA + " TEXT," + COLUMN_PHONE + " TEXT," +
+                COLUMN_EMAIL + " TEXT," + COLUMN_ALAMAT + " TEXT)";
+        db.execSQL(CREATE_KONTAK_TABLE);
     }
 
     @Override
     public void onUpgrade (SQLiteDatabase db, int oldVersion, int newVersion){
-        db.execSQL("DROP TABLE IF EXISTS kontak");
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
     }
 
-    public boolean insertContact (String nama, String phone, String email, String alamat){
+    public void TambahKontak(ModKontak modKontak){
         SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put("nama",nama);
-        contentValues.put("phone",phone);
-        contentValues.put("email",email);
-        contentValues.put("alamat",alamat);
-
-        db.insert("kontak",null,contentValues);
-        return true;
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NAMA, modKontak.getNama());
+        values.put(COLUMN_PHONE, modKontak.getPhone());
+        values.put(COLUMN_EMAIL, modKontak.getEmail());
+        values.put(COLUMN_ALAMAT, modKontak.getAlamat());
+        // Input Data
+        db.insert(TABLE_NAME, null, values);
+        db.close();
     }
 
-    public Cursor getData(int id){
-        SQLiteDatabase db = this.getReadableDatabase();
-        Cursor res = db.rawQuery("select * from kontak where id="+id+"",null);
-        return res;
+    public int UpdateData(ModKontak modKontak) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NAMA, modKontak.getNama());
+        values.put(COLUMN_PHONE, modKontak.getPhone());
+        values.put(COLUMN_EMAIL, modKontak.getEmail());
+        values.put(COLUMN_ALAMAT, modKontak.getAlamat());
+        //proses update tabel
+        return db.update(TABLE_NAME, values, COLUMN_ID + " = ?",new String[] { String.valueOf(modKontak.getIdUser()) });
     }
 
-    public int numberOfRow(){
-        SQLiteDatabase db = this.getReadableDatabase();
-        int numRows = (int) DatabaseUtils.queryNumEntries(db,MHS_TABLE_NAME);
-        return numRows;
+    public int HapusData(String idUser) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_NAME, COLUMN_ID + " = ?",new String[] {idUser });
     }
 
-    public ArrayList<String> getAllContacts(){
-        ArrayList<String> array_list = new ArrayList<String>();
+    public ModKontak getUserById(String id){
+        ModKontak modKontak=null;
+
+        String selectQuery = "SELECT  * FROM " + TABLE_NAME+" where "+COLUMN_ID+"="+id;
 
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor res = db.rawQuery("select * from kontak",null);
-        res.moveToFirst();
+        Cursor cursor = db.rawQuery(selectQuery, null);
 
-        while (res.isAfterLast() == false){
-            array_list.add(res.getString(res.getColumnIndex(MHS_COLUMN_NAMA)));
-            res.moveToNext();
+        if (cursor.moveToFirst()) {
+            do {
+                modKontak = new ModKontak();
+
+                modKontak.setIdUser(cursor.getString(cursor.getColumnIndex(COLUMN_ID)));
+                modKontak.setNama(cursor.getString(cursor.getColumnIndex(COLUMN_NAMA)));
+                modKontak.setPhone(cursor.getString(cursor.getColumnIndex(COLUMN_PHONE)));
+                modKontak.setEmail(cursor.getString(cursor.getColumnIndex(COLUMN_EMAIL)));
+                modKontak.setAlamat(cursor.getString(cursor.getColumnIndex(COLUMN_ALAMAT)));
+            } while (cursor.moveToNext());
         }
-        return array_list;
+
+        cursor.close();
+        db.close();
+
+        return modKontak;
+    }
+
+    public   Cursor AmbilSemuaData(){
+
+        String selectQuery = "SELECT  * FROM " + TABLE_NAME;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        // returning cursor
+        return cursor;
     }
 
 }
--- app/src/main/java/com/example/exercisesql_lite/MainActivity.java
+++ app/src/main/java/com/example/exercisesql_lite/MainActivity.java
@@ -17,46 +17,11 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    public final static String EXTRA_MESSAGE = "MESSAGE";
-    private ListView obj;
-    DBHelper mydb;
-    FloatingActionButton add;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        add = findViewById(R.id.add);
-        add.setOnClickListener(view ->
-                startActivity(new Intent(this, CreateKontak.class)));
-
-        mydb = new DBHelper(this);
-        ArrayList array_list = mydb.getAllContacts();
-        ArrayAdapter arrayAdapter = new
-                ArrayAdapter(this,android.R.layout.simple_list_item_1,array_list);
-
-        obj = (ListView)findViewById(R.id.listView1);
-        obj.setAdapter(arrayAdapter);
-        obj.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-            @Override
-            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-                int id_To_Search = arg2 + 1;
-
-                Bundle dataBundle = new Bundle();
-                dataBundle.putInt("id", id_To_Search);
-                Intent intent = new Intent(getApplicationContext(),CreateKontak.class);
-
-                intent.putExtras(dataBundle);
-                startActivity(intent);
-            }
-        });
     }
 
-    public boolean onKeyDown(int keycode, KeyEvent event) {
-        if (keycode == KeyEvent.KEYCODE_BACK) {
-            moveTaskToBack(true);
-        }
-        return super.onKeyDown(keycode, event);
-    }
 }
